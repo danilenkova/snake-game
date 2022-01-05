7d2995dcee1bda1478e1f941ddfe64c14f70bf5d
Yuliia Danilenkova <82413695+danilenkova@users.noreply.github.com>
2022-01-05 16:15:00 +0200

    make it better

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   connectionString:
-    "postgres://yshytjczzueurj:e4c408a3321b558bb2ca87b7eb19bfb5a613714f6a29afe0a3a3d3c3f5643515@ec2-54-170-163-224.eu-west-1.compute.amazonaws.com:5432/d6ar8s7j0ra5fs",
+    "postgres://aywgdrscuqblke:02c5efbed47f402628cac31ae1f1b8c5775e839a5af89fe92968197dfe76957a@ec2-52-215-22-82.eu-west-1.compute.amazonaws.com:5432/d690vtltiaenqk",
   ssl: {
     rejectUnauthorized: false,
   },
